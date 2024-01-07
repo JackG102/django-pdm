d0acc695a65e956634576727c34cdc7ec7280504
Jack Garratt <jack.garratt@icfnext.com>
2024-01-07 16:18:17 -0500

    Created initial sketch of Resource model

--- resource_library/admin.py
+++ resource_library/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# What shouldd our Resource model have?
+# Title
+# Link
+# Slug
+# Author
+# Resource Type
--- resource_library/models.py
+++ resource_library/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Resource(models.Model):
+  # Title
+  title = models.CharField(max_length=200)
+  # Link
+  link = models.URLField()
+  # Slug
+  slug = models.SlugField(unique=True, max_length=200, verbose_name="Slug")
+  # Author
+  author = models.CharField(max_length=200)
+  # Resource Type
+  RESOURCE_TYPES = {
+    "pdf": "PDF",
+    "video": "Video",
+    "podcast": "Podcast",
+    "website": "Website",
+    "book": "Book"
+  }
+  type = models.CharField(max_length=1, choices=RESOURCE_TYPES)
+  # Date Published
+  pub_date = models.DateField("date published")
