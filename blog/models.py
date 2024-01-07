cc5c3bdf38d72dec4afe63684588040f47ca9a12
Jack Garratt <jack.garratt@icfnext.com>
2024-01-07 06:40:22 -0500

    Wrote initial Blog model

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,14 @@ from ckeditor.fields import RichTextField
 
 # Define Blog Models Here
 class Blog:
-
+  # Title
+  title = models.CharField(max_length=200)
+  # Date Published
+  pub_date = models.DateField("date published")
+  # Body
+  body = models.TextField()
+  # Slug
+  slug = models.SlugField(unique=True, max_length=200, verbose_name="Slug")
 
   def __str__(self):
-    # Title
-    title = models.CharField(max_length=200)
-    # Date Published
-    pub_date = models.DateField("date published")
-    # Body
-
+    return self.title
