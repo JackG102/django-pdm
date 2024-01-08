645f6206eecf2fd2817f67f3c49555198a9b2e36
Jack Garratt <jack.garratt@icfnext.com>
2024-01-07 19:24:30 -0500

    Return title as name of object in admin menu

--- resource_library/models.py
+++ resource_library/models.py
@@ -7,7 +7,7 @@ class Resource(models.Model):
   # Title
   title = models.CharField(max_length=200)
   # Link
-  link = models.URLField()
+  link = models.URLField(null=True, blank=True)
   # Slug
   slug = models.SlugField(unique=True, max_length=200, verbose_name="Slug")
   # Author
@@ -25,3 +25,6 @@ class Resource(models.Model):
   type = models.CharField(choices=RESOURCE_TYPES, max_length=50)
   # Date Published
   pub_date = models.DateField("date published")
+
+  def __str__(self):
+    return self.title
