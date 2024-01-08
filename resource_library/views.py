6bc7f669c1d548c15ace389a48a2620bb44c883a
Jack Garratt <jack.garratt@icfnext.com>
2024-01-07 19:33:50 -0500

    Created Resource Library views

--- resource_library/views.py
+++ resource_library/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Resource
 
 # Create your views here.
+# Define Blog views
+class ResourceDetailView(generic.DetailView):
+  model = Resource
+  template_name = "resource_libary/detail.html"
+
+class ResourceIndexView(generic.ListView):
+  model = Resource
+  template_name = "resource_library/index.html"
+  paginate_by = 2
+  context_object_name = "resources"
